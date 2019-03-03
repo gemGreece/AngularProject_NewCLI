9a4d22d1e2337adc9b93bfaea5dac34ce741cb09
gemGreece <scarlll@hotmail.com>
2019-03-03 21:39:55 +0200

    Angular Routing --> Problem

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
+
 import { DishService } from './services/dish.service';
 import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     DishdetailComponent,
-    FooterComponent
+    FooterComponent,
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [DishService],
   bootstrap: [AppComponent]
