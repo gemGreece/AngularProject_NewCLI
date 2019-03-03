cbaaa30cbe66fce81c9069a4db6b382a391f93c8
gemGreece <scarlll@hotmail.com>
2019-03-03 19:22:06 +0200

    Angular Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { DishService } from './services/dish.service';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent
+    DishdetailComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
